88084b8e2faa9730c44e14bd58e0b0d9a0e61a32
Thinh Quoc LUU <quocthinhluu97@gmail.com>
2020-10-30 10:32:55 +0700

    Add project files.

---
+++ Controllers/AppController.cs
@@ -0,0 +1,50 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact us";
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("quocthinhluu@vnvn.vn", model.Subject, model.Message);
+                ViewBag.UserMessage = "Mail sent";
+                ModelState.Clear();
+            }
+            return View();
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            ViewBag.Title = "About us";
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/index.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    console.log("Hello Pluralsight");
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var buyButton = $("#buyButton");
+    buyButton.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var loginToggle = $("#loginToggle");
+    var popupForm = $(".popup-form");
+
+    loginToggle.on("click", function () {
+        popupForm.fadeToggle(200);
+    });
+});
